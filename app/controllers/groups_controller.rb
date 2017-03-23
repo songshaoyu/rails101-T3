f5b90b585037440de4289ace29280bea3d41584c
songshaoyu <ahu8756@gmail.com>
2017-03-23 14:05:25 +0800

    implement crud action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,30 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   def create
     @group = Group.new(group_params)
     @group.save
+    redirect_to groups_path
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    flash[:notice] = "Update Success"
     redirect_to groups_path
   end
 
